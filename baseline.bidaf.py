6bdc1bdd0a776f485a7c13176d737c1f57b791fb
Amar Oukaci <amaroukaci@MacBook-Pro-de-Amar.local>
2018-05-11 17:56:49 +0200

    bidaf batch_size 128

--- baseline.bidaf.py
+++ baseline.bidaf.py
@@ -431,6 +431,6 @@ if __name__ == '__main__':
     # print(x.output_shapes, a)
 
     machine = Baseline(train_dataset, val_dataset,
-                       embedding, vocabulary, batch_size=16)
+                       embedding, vocabulary, batch_size=128)
     machine.build()
     machine.train(10)
